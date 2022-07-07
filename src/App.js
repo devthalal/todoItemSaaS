92e5baa061a3294b271b892720b167d5b0f3ab04
devthalal <thalal.hassan@neoito.com>
2022-07-07 12:10:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import todoItemSaaS from './todoItemSaaS'
+
+export default function App(props) {
+  return (
+    <div>
+      <todoItemSaaS item="sdfsdf" id="sdfsdf" />
+    </div>
+  )
+}
